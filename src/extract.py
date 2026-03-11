3d461deebc3fe2b6b72bbeb44b2df5264735c1c8
AtimaAnand <anandatima@gmail.com>
2026-03-11 12:44:51 +0100

    add extract module

---
+++ src/extract.py
@@ -0,0 +1,9 @@
+import requests
+import pandas as pd
+
+def extract_data():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    data = response.json()
+    df = pd.DataFrame(data)
+    return df
