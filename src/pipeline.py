8c69db4e8c49b5a8449f14f1ed909aee4b4b2e23
AtimaAnand <anandatima@gmail.com>
2026-03-11 12:58:24 +0100

    connect ETL pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,1 +1,16 @@
-print("Starting ETL pipeline")
+from extract import extract_data
+from transform import transform_data
+from load import load_data
+
+def run_pipeline():
+
+    df = extract_data()
+
+    df_clean = transform_data(df)
+
+    load_data(df_clean)
+
+    print("ETL pipeline completed")
+
+if __name__ == "__main__":
+    run_pipeline()
